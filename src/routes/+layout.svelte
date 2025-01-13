fa3edd48b1fa88f0810987bf0062681f657fa9c4
slohth <hellspawnp@gmail.com>
2025-01-13 20:26:22 +0000

    about page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
     <div class="bg-white h-[6dvh] drop-shadow-[0_1px_1px_rgb(0,0,0,0.2)] flex items-center">
         <!-- Logo -->
         <div class="h-[50%]">
-            <img src="/favicon.png" alt="Favicon" class="h-full ml-10 ">
+            <img src="/favicon.png" alt="Favicon" class="h-full ml-10">
         </div>
         <div class="font-[Minecraft] text-3xl">
             <p class="ml-3 text-[#808080]">PotatoSkin<span class="text-[#a6a6a6]">.art</span></p>
@@ -23,7 +23,7 @@
         <!-- Pages -->
         <div class="ml-32 flex gap-16 text-xl text-[#a6a6a6] font-light">
             {#each pages as p}
-                <div class={$page.url.pathname === p.href ? "font-medium text-[#666666]" : ""}> <!-- bold selected page -->
+                <div class={$page.url.pathname === p.href ? "font-bold text-[#666666]" : ""}> <!-- bold selected page -->
                     <a href={p.href}>{p.name}</a>
                 </div>
             {/each}
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,57 @@
-about
+<div class="flex flex-col items-center h-full">
+    <div class="my-[5dvh] w-[60%] h-full bg-white rounded-[60px] border-[2px] border-opacity-35">
+        <!-- About content -->
+        <div class="mt-[4dvh] mx-[12dvh] text-[#404040] text-xl">
+            <!-- Header -->
+            <div>
+                <div class="flex items-center">
+                    <div><img src="/favicon.png" alt="Favicon" class="h-12"></div>
+                    <div><p class="font-bold text-4xl ml-5">About PotatoSkin</p></div>
+                </div>
+                <div class="mt-[1dvh] bg-black bg-opacity-10 min-h-[2px] w-full"></div>
+                <p class="mt-[2dvh] text-[#a6a6a6] text-[18px] font-light"><i>"Tools to make beautiful skins, that could run on a potato."</i></p>
+            </div>
+            <!-- About -->
+            <div class="mt-[2dvh]">
+                <p>
+                    PotatoSkin is a skin editor built to be quick, responsive and easy to use. We have a large range of creative tools which makes our site
+                    <span class="font-extrabold">powerful for artists</span> and <span class="font-extrabold">powerful for developers</span>.<br>
+                    <br>
+                    This site was built with artists at front of mind, and it’s completely free to use. No AI, no ads, and no paywalls or hidden fees.<br>
+                    Open-source, free, forever. That’s our promise.
+                </p>
+            </div>
+            <!-- Why -->
+            <div class="mt-[4dvh]">
+                <p class="font-bold text-2xl">Why use PotatoSkin?</p>
+                <p class="mt-[2dvh]">
+                    With our features and accessibility, you won’t find a more comprehensive skin editor out there.
+                    We offer tools to make beautiful art easily, without any of the bloat or clunkiness that you’ll see elsewhere.<br>
+                    <br>
+                    Here’s a non-exhaustive list of some of our features:<br>
+                    <br>
+                </p>
+                <ul class="ml-[3dvh]">
+                    <li><p><span class="font-extrabold">Beautiful UI: </span><i>Navigate our site easily, and look good doing it!</i></p></li>
+                    <li><p><span class="font-extrabold">Layer System: </span><i>Use as many layers as you want, so you won’t have to risk painting over something by mistake!</i></p></li>
+                    <li><p><span class="font-extrabold">Opacity: </span><i>Wish you could add transparent glasses? Now you can with ease!</i></p></li>
+                    <li><p><span class="font-extrabold">Gradients: </span><i>Don’t suffer trying to manually make beautiful-looking gradients by hand.</i></p></li>
+                    <li><p><span class="font-extrabold">Custom Pallets: </span><i>Create your own custom pallets with as many swatches as you like!</i></p></li>
+                    <li><p><span class="font-extrabold">Flat View: </span><i>Want to edit directly onto the texture image? No problem!</i></p></li>
+                    <li><p><span class="font-extrabold">Property: </span><i>Get the data for this player head or to apply this skin to an NPC with one easy click.</i></p></li>
+                </ul>
+            </div>
+            <!-- Support -->
+            <div class="mt-[4dvh]">
+                <p class="font-bold text-2xl">Support Us ❤️</p>
+                <p class="mt-[2dvh]">
+                    If you love our site as much as we do, please consider supporting us! If you’re an artist or know other artists,
+                    please share this site with them and spread the potato love.<br>
+                    <br>
+                    If you are fortunate enough to donate, please consider doing so here.<br>
+                    This site is ran completely non-profit. Any excess funds will be donated to charity.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
